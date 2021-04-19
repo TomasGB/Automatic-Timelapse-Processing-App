7d6bc9856f8e1427de60f172440a782362140fdb
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2021-04-18 22:46:39 -0300

    made a devicePreview function and button for the UI

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ import timelapse
 import threading 
 from cv2 import cv2
 
+def devicePreview():
+    cap = cv2.VideoCapture(int(Device_Entry.get()),cv2.CAP_DSHOW)
+
+    if (cap.isOpened() == False):
+        print('No video device')
+        progress_label.config(text="There is no device, please try other number.", font="Helvetica 9")
+        return quit
+    
+    progress_label.config(text=" ", font="Helvetica 9")
+    while(True):
+            ret, frame = cap.read()
+            cv2.imshow('press "q" to close', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
 def FuncStart():
 
     def processing():
@@ -34,17 +52,18 @@ def FuncStart():
     
 
 window = Tk()
-window.geometry("525x500")
-window.title("Automatic Timelapse Processor - Tomas Gomez Bermudez")
+window.geometry("335x550")
+window.title("Timelapsing! - Tomas Gomez Bermudez")
+window.iconbitmap('imgs/timelapsingicon.ico')
 window.config(background="#151515")
 
-title = Label(window, text="Automatic Timelapse Processor", padx=100, pady=30, bg="#151515", foreground="#DE4C12", font="Helvetica 16 bold")
-title.grid(column="0", row="0", columnspan="3")
+title = Label(window, text="Timelapsing!", padx=100, pady=30, bg="#151515", foreground="#DE4C12", font="Helvetica 16 bold")
+title.grid(column="0", row="0")
 
 #Frame Create Timelapse
 
 groupCreate = LabelFrame(window, text="Create Timelapse", labelanchor=N, padx=25, pady=10, bg="#151515", foreground="#FFFFFF", font="Helvetica 12 bold")
-groupCreate.grid(column="1", row="3")
+groupCreate.grid(column="0", row="3")
 
 Duration_label = Label(groupCreate, text="Duration (minutes)" ,pady=5, bg="#151515", foreground="#FFFFFF", font="Helvetica 10")
 Duration_label.pack()
@@ -71,12 +90,18 @@ Device_label.pack()
 Device_Entry = Entry(groupCreate, width="35")
 Device_Entry.pack()
 
-progress_label = Label(window, text=" ", bg="#151515", foreground="#FFFFFF", font="Helvetica 14")
-progress_label.grid(column="0",row="5",columnspan="3")
+progress_label = Label(window, text=" ", bg="#151515",pady=10, foreground="#FFFFFF", font="Helvetica 14")
+progress_label.grid(column="0",row="6",columnspan="3")
 
 beginButton = ttk.Button(window, text="Start", command= FuncStart)
-beginButton.grid(column="0", row="4",columnspan="3", pady=25 )
+beginButton.grid(column="0", row="4",columnspan="1", pady=10 )
 
 progress = ttk.Progressbar(groupCreate, length = 100, mode = 'indeterminate')
+
+previewButton = ttk.Button(window, text="Preview", command= devicePreview)
+previewButton.grid(column="0", row="5",columnspan="1", pady=5 )
+
+Name_label = Label(window, text="Developed by Tomas Gomez Bermudez", bg="#151515",pady=5, foreground="#FFFFFF", font="Helvetica 9")
+Name_label.grid(column="0",row="7")
 
 window.mainloop()
